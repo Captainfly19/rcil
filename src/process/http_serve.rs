b6f983f3886e152406b2da27a6eb80d83298a50c
bchng <1226274453@qq.com>
2024-11-29 12:14:26 +0800

    http tower finish

--- src/process/http_serve.rs
+++ src/process/http_serve.rs
@@ -1,13 +1,14 @@
-use std::{net::SocketAddr, os::macos::raw::stat, path::PathBuf, sync::Arc};
 use anyhow::Result;
 use axum::{
-    extract::{Path,State}, 
-    http::StatusCode, 
-    routing::get, 
+    extract::{Path, State},
+    http::StatusCode,
+    routing::get,
     Router,
 };
+use std::{net::SocketAddr, path::PathBuf, sync::Arc};
+use tower_http::services::ServeDir;
 use tracing::{info, warn};
-
+ 
 #[derive(Debug)]
 struct HttpServeState {
     path:PathBuf,
@@ -16,8 +17,17 @@ struct HttpServeState {
 pub async fn process_http_serve(path:PathBuf,port:u16) -> Result<()> {
     let addr = SocketAddr::from(([0,0,0,0],port));
     info!("Serving {:?} on port {}",path,addr);
-    let state = HttpServeState{path};
-    let router = Router::new().route("/*path", get(file_handler))
+    let state = HttpServeState{path:path.clone()};
+
+    let dir_service = ServeDir::new(path)
+        .append_index_html_on_directories(true)
+        .precompressed_gzip()
+        .precompressed_br()
+        .precompressed_deflate()
+        .precompressed_zstd();
+    
+    let router = Router::new()
+    .nest_service("/", dir_service)
     .with_state(Arc::new(state));
 
     let listener = tokio::net::TcpListener::bind(addr).await?;
@@ -49,5 +59,20 @@ async fn file_handler(
             }
         }
 
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_file_handler() {
+        let state = Arc::new(HttpServeState {
+            path: PathBuf::from("."),
+        });
+        let (status, content) = file_handler(State(state), Path("Cargo.toml".to_string())).await;
+        assert_eq!(status, StatusCode::OK);
+        assert!(content.trim().starts_with("[package]"));
     }
 }
